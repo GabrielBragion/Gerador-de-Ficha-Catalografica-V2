644ce6e10e22f3dc9ec3ea2113486a31df2b7579
Gabriel Bragion <gabriel.bragion.micco@gmail.com>
2025-01-31 09:52:42 +0000

    first - estrutura base criada

---
+++ front-end/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer>
+			<p>Desenvolvido por Gabriel Bragion de Micco</p>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header>
+			<h1>Gerador Automatico de Ficha Catalografica</h1>
+		</header>
+	);
+};
+
+export default Header;
